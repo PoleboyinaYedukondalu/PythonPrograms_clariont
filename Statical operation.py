988a729b33a1e0afcef71d5c9cbb33a40ec20b56
Yedukondalu Poleboyina <116720252+PoleboyinaYedukondalu@users.noreply.github.com>
2023-11-20 10:17:48 +0530

    Add files via upload

--- Statical operation.py
+++ Statical operation.py
@@ -2,9 +2,16 @@ import numpy as np
 # NumPy provides various statistical functions.
 
 arr = np.array([1, 2, 3, 4, 5])
+arr1 = np.array([1,2,3,4,5])
 # Mean :
 mean_value = np.mean(arr)
-print(mean_value)
+print(f"mean of {mean_value}  is :\n",mean_value)
 # Sum :
 sum_value = np.sum(arr)
-print(sum_value)
+print(f"sum of {arr} is : \n",sum_value)
+# multiplicationm:
+multiplication = np.multiply(arr,arr1)
+print(f"multiplication of {arr} & {arr1} is :\n",multiplication)
+
+division_arr = np.divide(arr,arr1)
+print(f"division of {arr} & {arr1} is : \n",division_arr)
